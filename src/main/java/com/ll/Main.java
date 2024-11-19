a43edf78a22700ff0cbddbc73631f6d1215bae06
universechoi <skye.of.universe@gmail.com>
2024-11-19 14:14:16 +0900

    App Class

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,9 +1,16 @@
 package com.ll;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        App app = new App();
+        app.run();
+    }
+}
+
+class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        System.out.print("명령) ");
     }
 }
