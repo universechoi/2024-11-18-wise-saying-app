10cc636200f88f6bcbb9ec7d895568ae055bc84f
universechoi <skye.of.universe@gmail.com>
2024-11-19 14:26:30 +0900

    Scanner

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app = new App();
@@ -11,6 +13,13 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        Scanner scanner = new Scanner(System.in);
+
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+        scanner.close();
     }
 }
