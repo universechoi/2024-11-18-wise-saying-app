c64d2655f6262df81aac2e6fab0b8797351378ab
universechoi <skye.of.universe@gmail.com>
2024-11-19 16:40:08 +0900

    Register

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -15,12 +15,24 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
 
+        int count = 1;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
             if (cmd.equals("종료")) {
                 break;
+            }
+            if (cmd.equals("등록")) {
+                System.out.print("명언 : ");
+                String wiseSaying = scanner.nextLine();
+
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
+
+                System.out.println(count + "번 명언이 등록되었습니다.");
+                count++;
             }
         }
         scanner.close();
