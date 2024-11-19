5d9f24977ec7efc7279359a5dc680bff637ea9f1
universechoi <skye.of.universe@gmail.com>
2024-11-19 14:42:46 +0900

    Repeat and Exit

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -15,11 +15,14 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("명령) ");
-        String cmd = scanner.nextLine();
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine();
 
-        System.out.println("입력된 명령어 : %s".formatted(cmd));
-
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
         scanner.close();
     }
 }
