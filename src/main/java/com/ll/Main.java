1583b32ec4d8cac2636b10c51e8de7170fa8f698
universechoi <skye.of.universe@gmail.com>
2024-11-20 16:07:48 +0900

    List

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,6 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -17,6 +18,8 @@ class App {
 
         int count = 1;
 
+        ArrayList<String> wiseSayingList = new ArrayList<>();
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -24,7 +27,7 @@ class App {
             if (cmd.equals("종료")) {
                 break;
             }
-            if (cmd.equals("등록")) {
+            else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String wiseSaying = scanner.nextLine();
 
@@ -32,7 +35,18 @@ class App {
                 String author = scanner.nextLine();
 
                 System.out.println(count + "번 명언이 등록되었습니다.");
+
+                String addForm = count + " / " + author + " / " + wiseSaying;
+                wiseSayingList.add(addForm);
+
                 count++;
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+
+                for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
+                    System.out.println(wiseSayingList.get(i));
+                }
             }
         }
         scanner.close();
