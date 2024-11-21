6030d9958e2a67dfc2a4a9f5b157417339063e5c
universechoi <skye.of.universe@gmail.com>
2024-11-21 15:41:22 +0900

    Using Class

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -16,9 +16,9 @@ class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        int count = 1;
+        int lastId = 0;
 
-        ArrayList<String> wiseSayingList = new ArrayList<>();
+        ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
 
         while (true) {
             System.out.print("명령) ");
@@ -26,29 +26,40 @@ class App {
 
             if (cmd.equals("종료")) {
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String wiseSaying = scanner.nextLine();
+                String content = scanner.nextLine();
 
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
 
-                System.out.println(count + "번 명언이 등록되었습니다.");
+                int id = ++lastId;
 
-                String addForm = count + " / " + author + " / " + wiseSaying;
-                wiseSayingList.add(addForm);
+                wiseSayingList.add(new WiseSaying(id, content, author));
+                System.out.println(id + "번 명언이 등록되었습니다.");
 
-                count++;
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
 
                 for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
-                    System.out.println(wiseSayingList.get(i));
+                    WiseSaying value = wiseSayingList.get(i);
+                    System.out.println(value.id + " / " + value.author + " / " + value.content);
                 }
             }
         }
         scanner.close();
+    }
+
+    class WiseSaying {
+        int id;
+        String content;
+        String author;
+
+        WiseSaying(int id, String content, String author) {
+            this.id = id;
+            this.content = content;
+            this.author = author;
+        }
     }
 }
