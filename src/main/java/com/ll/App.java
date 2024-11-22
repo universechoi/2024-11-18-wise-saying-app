8eaee6268a0595fdcc260af4634462bbfb1d9d31
universechoi <skye.of.universe@gmail.com>
2024-11-22 14:07:20 +0900

    Add sample data and list reversed

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,13 +1,14 @@
 package com.ll;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
     private int lastId;
-    private final ArrayList<WiseSaying> wiseSayingList;
+    private final List<WiseSaying> wiseSayingList;
 
     public App() {
         scanner = new Scanner(System.in);
@@ -17,6 +18,9 @@ public class App {
 
     public void run() {
         System.out.println("== 명언 앱 ==");
+
+        makeSampleData();
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
@@ -60,7 +64,14 @@ public class App {
         scanner.close();
     }
 
+    private void makeSampleData() {
+        addWiseSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
+        addWiseSaying("삶이 있는 한 희망은 있다.", "키케로");
+    }
+
     private void addWiseSaying(String content, String author) {
+        ++lastId;
+
         wiseSayingList.add(new WiseSaying(lastId, content, author));
         System.out.println(lastId + "번 명언이 등록되었습니다.");
     }
@@ -72,8 +83,6 @@ public class App {
         System.out.print("작가 : ");
         String author = scanner.nextLine();
 
-        ++lastId;
-
         addWiseSaying(content, author);
     }
 
@@ -81,7 +90,7 @@ public class App {
         System.out.println("번호 / 작가 / 명언");
         System.out.println("----------------------");
 
-        for (WiseSaying wiseSaying : wiseSayingList) {
+        for (WiseSaying wiseSaying : wiseSayingList.reversed()) {
             if (wiseSaying != null) {
                 System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getContent());
             }
